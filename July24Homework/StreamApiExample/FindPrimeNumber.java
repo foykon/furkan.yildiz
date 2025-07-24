0faac53ad54f7b54a12341e7fd5a09616dc9a990
foykon <foykon9@gmail.com>
2025-07-24 18:32:00 +0300

    another inline return for prime number problem

--- July24Homework/StreamApiExample/FindPrimeNumber.java
+++ July24Homework/StreamApiExample/FindPrimeNumber.java
@@ -25,14 +25,20 @@ public class FindPrimeNumber {
 
     private List<Integer> getPrimeNumbers() {
         return numbers.stream().filter(f -> {
-            if(f == 1) return false;
-            if(f == 2) return true;
-            if(f % 2 == 0) return false;
-            for(int i = 3; i <= Math.sqrt(f); i++){
-                if(f % i == 0) return false;
+            if (f == 1) return false;
+            if (f == 2) return true;
+            if (f % 2 == 0) return false;
+            int limit = (int) Math.sqrt(f);
+            for (int i = 3; i <= limit; i += 2) {
+                if (f % i == 0) return false;
             }
             return true;
         }).collect(Collectors.toList());
+        //return numbers.stream()
+        //        .filter(f -> f > 1 && (f == 2 || (f % 2 != 0 && IntStream.rangeClosed(3, (int)Math.sqrt(f))
+        //            .filter(i -> i % 2 != 0)
+        //            .noneMatch(i -> f % i == 0))))
+        //        .collect(Collectors.toList());
     }
 
 }
