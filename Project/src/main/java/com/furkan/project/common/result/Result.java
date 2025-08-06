7459b0bbfed8ce7b842ea447b1c1ff50cba53c00
foykon <foykon9@gmail.com>
2025-08-06 17:16:11 +0300

    result class

---
+++ Project/src/main/java/com/furkan/project/common/result/Result.java
@@ -0,0 +1,15 @@
+package com.furkan.project.common.result;
+
+public class Result {
+    private boolean success;
+    private String message;
+
+    public Result(boolean success) {
+        this.success = success;
+    }
+
+    public Result(boolean success, String message) {
+        this(success);
+        this.message = message;
+    }
+}
