6aca4bb52e7ba834a347c33bfbb8bd443a571150
foykon <foykon9@gmail.com>
2025-08-12 16:25:39 +0300

    swaggerconfig for auth button

---
+++ Project/src/main/java/com/furkan/project/common/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.furkan.project.common.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.*;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.*;
+import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info().title("Movie Portal API").version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
